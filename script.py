1255651802dfaa8f8f8d1b8cd0ecaef091c117ad
wupaloupa <wupaloupa@naver.com>
2023-06-09 05:13:51 +0900

    add roulette

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import random
+import asyncio
 
 multiply2 = Element("multiply2")
 multiply3 = Element("multiply3")
@@ -7,10 +8,11 @@ multiply10 = Element("multiply10")
 multiply20 = Element("multiply20")
 currentscore = Element("score")
 log=Element("log")
+imgbox = Element("imgbox")
 score=100
 
 
-def gamestart(*args):
+async def gamestart(*args):
     global score
     mp2=0
     mp3=0
@@ -18,7 +20,7 @@ def gamestart(*args):
     mp10=0
     mp20=0
     log.element.innerText=""
-
+    imgbox.element.innerHTML = ""
    
     if multiply2.value != "":
         mp2=int(multiply2.value)
@@ -46,26 +48,26 @@ def gamestart(*args):
         score=score-mp20-mp10-mp5-mp3-mp2
         if x<=4:
             result = mp20*20
+            imgbox.element.innerHTML = "<video src=\"20.webm\" id=\"video\" autoplay></video>"
         elif x<=12:
             result = mp10*10
+            imgbox.element.innerHTML = "<video src=\"10.webm\" id=\"video\" autoplay></video>"
         elif x<=28:
             result = mp5*5
+            imgbox.element.innerHTML = "<video src=\"5.webm\" id=\"video\" autoplay></video>"
         elif x<=52:
             result = mp3*3
+            imgbox.element.innerHTML = "<video src=\"3.webm\" id=\"video\" autoplay></video>"
         elif x<=100:
             result = mp2*2
+            imgbox.element.innerHTML = "<video src=\"2.webm\" id=\"video\" autoplay></video>"
         score=score+result
-        
+    await asyncio.sleep(6.5)
     currentscore.element.innerText=f"현재점수:{score}"
-
+    
     
     multiply2.clear()
     multiply3.clear()
     multiply5.clear()
     multiply10.clear()
     multiply20.clear()
-
-
-
-    
-
